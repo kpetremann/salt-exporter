700152bce4b303896f5261e4ad23eb36e63565f6
Kevin Petremann <kpetrem@gmail.com>
2023-05-14 22:35:16 +0200

    fix: remove salt-live debug logs

--- cmd/salt-live/main.go
+++ cmd/salt-live/main.go
@@ -22,18 +22,21 @@ func printVersion() {
 }
 
 func main() {
-	f, err := tea.LogToFile("debug.log", "debug")
-	if err != nil {
-		fmt.Println("fatal:", err)
-		os.Exit(1)
-	}
-	defer f.Close()
-
 	maxItems := flag.Int("max-events", 1000, "maximum events to keep in memory")
 	bufferSize := flag.Int("buffer-size", 1000, "buffer size in number of events")
 	filter := flag.String("hard-filter", "", "filter when received (filtered out events are discarded forever)")
 	versionCmd := flag.Bool("version", false, "print version")
+	debug := flag.Bool("debug", false, "enable debug mode (log to debug.log)")
 	flag.Parse()
+
+	if *debug {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			fmt.Println("fatal:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
 
 	if *versionCmd {
 		printVersion()
