3f67266f8621952c03ce816372647ca8b7d3488c
Kevin Petremann <kpetrem@gmail.com>
2023-06-27 23:00:27 +0200

    feat: be able to enable/disable metrics
    The goal is to be able to enable or disable any metric we want
    independently.
    
    Some metrics can be disabled for cardinality scaling.

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -2,12 +2,8 @@ package metrics
 
 import (
 	"context"
-	"strconv"
 
-	"github.com/kpetremann/salt-exporter/internal/filters"
 	"github.com/kpetremann/salt-exporter/pkg/event"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/rs/zerolog/log"
 )
 
@@ -18,50 +14,35 @@ func boolToFloat64(b bool) float64 {
 	return 0.0
 }
 
-func ExposeMetrics(ctx context.Context, eventChan <-chan event.SaltEvent, metricsConfig Config) {
-	newJobCounter := promauto.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "salt_new_job_total",
-			Help: "Total number of new job processed",
-		},
-		[]string{"function", "state", "success"},
-	)
-	responsesCounter := promauto.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "salt_responses_total",
-			Help: "Total number of response job processed",
-		},
-		[]string{"minion", "success"},
-	)
-	functionResponsesCounter := promauto.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "salt_function_responses_total",
-			Help: "Total number of response per function processed",
-		},
-		[]string{"function", "state", "success"},
-	)
-	lastFunctionStatus := promauto.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "salt_function_status",
-			Help: "Last function/state success, 0=Failed, 1=Success",
-		},
-		[]string{"minion", "function", "state"},
-	)
+func eventToMetrics(event event.SaltEvent, r Registry) {
+	switch event.Type {
+	case "new":
+		state := event.ExtractState()
+		r.IncreaseNewJobTotal(event.Data.Fun, state, true)
+		r.IncreaseExpectedResponsesTotal(event.Data.Fun, state, float64(event.TargetNumber))
 
-	scheduledJobReturnCounter := promauto.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "salt_scheduled_job_return_total",
-			Help: "Total number of scheduled job response",
-		},
-		[]string{"function", "state", "success"},
-	)
-	expectedResponsesNumber := promauto.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "salt_expected_responses_total",
-			Help: "Total number of expected minions responses",
-		},
-		[]string{"function", "state"},
-	)
+	case "ret":
+		state := event.ExtractState()
+		success := event.Data.Success
+
+		if event.IsScheduleJob {
+			// for scheduled job, when the states in the job actually failed
+			// - the global "success" value is always true
+			// - the substate success is false, and the global retcode is > 0
+			// using retcode could be enough, but in case there are other corner cases, we combine both values
+			success = event.Data.Success && (event.Data.Retcode == 0)
+			r.IncreaseScheduledJobReturnTotal(event.Data.Fun, state, success)
+		} else {
+			r.IncreaseResponseTotal(event.Data.Id, success) // TODO: move outside the if?
+			r.IncreaseFunctionResponsesTotal(event.Data.Fun, state, success)
+		}
+
+		r.SetFunctionStatus(event.Data.Id, event.Data.Fun, state, success)
+	}
+}
+
+func ExposeMetrics(ctx context.Context, eventChan <-chan event.SaltEvent, config Config) {
+	registry := NewRegistry(config)
 
 	for {
 		select {
@@ -69,59 +50,14 @@ func ExposeMetrics(ctx context.Context, eventChan <-chan event.SaltEvent, metric
 			log.Info().Msg("stopping event listener")
 			return
 		case event := <-eventChan:
-			if metricsConfig.Global.Filters.IgnoreTest && event.IsTest || metricsConfig.Global.Filters.IgnoreMock && event.IsMock {
+			if config.Global.Filters.IgnoreTest && event.IsTest {
+				return
+			}
+			if config.Global.Filters.IgnoreMock && event.IsMock {
 				return
 			}
 
-			switch event.Type {
-			case "new":
-				state := event.ExtractState()
-				newJobCounter.WithLabelValues(event.Data.Fun, state, "true").Inc()
-				expectedResponsesNumber.WithLabelValues(event.Data.Fun, state).Add(float64(event.TargetNumber))
-			case "ret":
-				var success bool
-				state := event.ExtractState()
-				if event.IsScheduleJob {
-					// for scheduled job, when the states in the job actually failed
-					// - the global "success" value is always true
-					// - the substate success is false, and the global retcode is > 0
-					// using retcode could be enough, but in case there are other corner cases, we combine both values
-					success = event.Data.Success && (event.Data.Retcode == 0)
-					scheduledJobReturnCounter.WithLabelValues(
-						event.Data.Fun,
-						state,
-						strconv.FormatBool(success),
-					).Inc()
-				} else {
-					success = event.Data.Success
-
-					if metricsConfig.HealthMinions {
-						responsesCounter.WithLabelValues(
-							event.Data.Id,
-							strconv.FormatBool(success),
-						).Inc()
-					}
-
-					functionResponsesCounter.WithLabelValues(
-						event.Data.Fun,
-						state,
-						strconv.FormatBool(success),
-					).Inc()
-				}
-
-				// Expose state/func status if feature enabled and matching filters
-				if !metricsConfig.HealthMinions {
-					continue
-				}
-				if !filters.Match(event.Data.Fun, metricsConfig.SaltFunctionStatus.Filters.Functions) {
-					continue
-				}
-				log.Debug().Msg("function matches")
-				if !filters.Match(state, metricsConfig.SaltFunctionStatus.Filters.States) {
-					continue
-				}
-				lastFunctionStatus.WithLabelValues(event.Data.Id, event.Data.Fun, state).Set(boolToFloat64(success))
-			}
+			eventToMetrics(event, registry)
 		}
 	}
 }
